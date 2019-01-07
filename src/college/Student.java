d924b47475340becf2c5de88e709c0412a8341cf
Omkar Agrahar <interns@volken.in>
2019-01-07 11:58:10 +0530

    Program to create a collage by adding 3 departments in each department adding 3 students with deatils name dob email.

---
+++ src/college/Student.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package college;
+
+/**
+ *
+ * @author omkar
+ */
+import java.util.Date;
+
+public class Student {
+
+    String studentname;
+    Date dob;
+    String email;
+
+}
